099a6748acef2e52239e57b82b91a2ca3bc6db92
Liu-Yonghua <liuyonghua@westlake.edu.cn>
2026-08-25 16:15:59 +0800

    0.5.2 [Test]

--- src/mukip/__init__.py
+++ src/mukip/__init__.py
@@ -3,5 +3,5 @@ from .jvm_manager import initialize
 from .plot_1d import plot_1d
 from .plot_2d import plot_2d
 
-__version__ = "0.5.1"
+__version__ = "0.5.2"
 __all__ = ['initialize', 'KineticModel', 'plot_1d', 'plot_2d']
